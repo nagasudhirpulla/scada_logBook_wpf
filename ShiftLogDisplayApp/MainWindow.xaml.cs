38933511422141f5d07d48102f5383f73805d86c
nagasudhirpulla <nagasudhirpulla@gmail.com>
2019-11-26 21:25:33 +0530

    import export stats done

--- ShiftLogDisplayApp/MainWindow.xaml.cs
+++ ShiftLogDisplayApp/MainWindow.xaml.cs
@@ -80,20 +80,36 @@ namespace ShiftLogDisplayApp
             (string, double) minDem = StatUtils.GetMinValueInfo(demVals, true);
             // get WR-SR data
             List<(string, double)> wrSrVals = EdnaUtils.GetData(startTime, endTime, EdnaUtils.WrSrPnt);
-            (string, double) maxWrSr = StatUtils.GetMaxValueInfo(wrSrVals, true);
-            (string, double) minWrSr = StatUtils.GetMinValueInfo(wrSrVals, true);
+            List<(string, double)> exportVals = wrSrVals.Where(x => x.Item2 < 0).ToList();
+            List<(string, double)> importVals = wrSrVals.Where(x => x.Item2 > 0).ToList();
+            (string, double) maxWrSrEx = StatUtils.GetMaxValueInfo(exportVals, true);
+            (string, double) minWrSrEx = StatUtils.GetMinValueInfo(exportVals, true);
+            (string, double) maxWrSrImp = StatUtils.GetMaxValueInfo(importVals, true);
+            (string, double) minWrSrImp = StatUtils.GetMinValueInfo(importVals, true);
             // get WR-NR data
             List<(string, double)> wrNrVals = EdnaUtils.GetData(startTime, endTime, EdnaUtils.WrNrPnt);
-            (string, double) maxWrNr = StatUtils.GetMaxValueInfo(wrNrVals, true);
-            (string, double) minWrNr = StatUtils.GetMinValueInfo(wrNrVals, true);
+            exportVals = wrNrVals.Where(x => x.Item2 < 0).ToList();
+            importVals = wrNrVals.Where(x => x.Item2 > 0).ToList();
+            (string, double) maxWrNrEx = StatUtils.GetMaxValueInfo(exportVals, true);
+            (string, double) minWrNrEx = StatUtils.GetMinValueInfo(exportVals, true);
+            (string, double) maxWrNrImp = StatUtils.GetMaxValueInfo(importVals, true);
+            (string, double) minWrNrImp = StatUtils.GetMinValueInfo(importVals, true);
             // get WR-ER data
             List<(string, double)> wrErVals = EdnaUtils.GetData(startTime, endTime, EdnaUtils.WrErPnt);
-            (string, double) maxWrEr = StatUtils.GetMaxValueInfo(wrErVals, true);
-            (string, double) minWrEr = StatUtils.GetMinValueInfo(wrErVals, true);
+            exportVals = wrErVals.Where(x => x.Item2 < 0).ToList();
+            importVals = wrErVals.Where(x => x.Item2 > 0).ToList();
+            (string, double) maxWrErEx = StatUtils.GetMaxValueInfo(exportVals, true);
+            (string, double) minWrErEx = StatUtils.GetMinValueInfo(exportVals, true);
+            (string, double) maxWrErImp = StatUtils.GetMaxValueInfo(importVals, true);
+            (string, double) minWrErImp = StatUtils.GetMinValueInfo(importVals, true);
             // get WR IR data
             List<(string, double)> wrIrVals = EdnaUtils.GetData(startTime, endTime, EdnaUtils.WrIrPnt);
-            (string, double) maxWrIr = StatUtils.GetMaxValueInfo(wrIrVals, true);
-            (string, double) minWrIr = StatUtils.GetMinValueInfo(wrIrVals, true);
+            exportVals = wrIrVals.Where(x => x.Item2 < 0).ToList();
+            importVals = wrIrVals.Where(x => x.Item2 > 0).ToList();
+            (string, double) maxWrIrEx = StatUtils.GetMaxValueInfo(exportVals, true);
+            (string, double) minWrIrEx = StatUtils.GetMinValueInfo(exportVals, true);
+            (string, double) maxWrIrImp = StatUtils.GetMaxValueInfo(importVals, true);
+            (string, double) minWrIrImp = StatUtils.GetMinValueInfo(importVals, true);
 
             DataTable logBookDt = new DataTable();
             int numColumns = 8;
@@ -114,10 +130,22 @@ namespace ShiftLogDisplayApp
             newRow.ItemArray = new string[] { maxDem.Item2.ToString("F0"), TillMinsOnly(maxDem.Item1), minDem.Item2.ToString("F0"), TillMinsOnly(minDem.Item1), "", "", "", "" };
             logBookDt.Rows.Add(newRow);
             newRow = logBookDt.NewRow();
+            newRow.ItemArray = new string[] { "Export values", "", "", "", "", "", "", "" };
+            logBookDt.Rows.Add(newRow);
+            newRow = logBookDt.NewRow();
             newRow.ItemArray = new string[] { "Min WR-ER", "Max WR-ER", "Min WR-SR", "Max WR-SR", "Min WR-NR", "Max WR-NR", "Min IR", "Max IR" };
             logBookDt.Rows.Add(newRow);
             newRow = logBookDt.NewRow();
-            newRow.ItemArray = new string[] { minWrEr.Item2.ToString("F0"), maxWrEr.Item2.ToString("F0"), minWrSr.Item2.ToString("F0"), maxWrSr.Item2.ToString("F0"), minWrNr.Item2.ToString("F0"), maxWrNr.Item2.ToString("F0"), minWrIr.Item2.ToString("F0"), maxWrIr.Item2.ToString("F0") };
+            newRow.ItemArray = new string[] { maxWrErEx.Item2.ToString("F0"), minWrErEx.Item2.ToString("F0"), maxWrSrEx.Item2.ToString("F0"), minWrSrEx.Item2.ToString("F0"), maxWrNrEx.Item2.ToString("F0"), minWrNrEx.Item2.ToString("F0"), maxWrIrEx.Item2.ToString("F0"), minWrIrEx.Item2.ToString("F0") };
+            logBookDt.Rows.Add(newRow);
+            newRow = logBookDt.NewRow();
+            newRow.ItemArray = new string[] { "Import values", "", "", "", "", "", "", "" };
+            logBookDt.Rows.Add(newRow);
+            newRow = logBookDt.NewRow();
+            newRow.ItemArray = new string[] { "Min WR-ER", "Max WR-ER", "Min WR-SR", "Max WR-SR", "Min WR-NR", "Max WR-NR", "Min IR", "Max IR" };
+            logBookDt.Rows.Add(newRow);
+            newRow = logBookDt.NewRow();
+            newRow.ItemArray = new string[] { minWrErImp.Item2.ToString("F0"), maxWrErImp.Item2.ToString("F0"), minWrSrImp.Item2.ToString("F0"), maxWrSrImp.Item2.ToString("F0"), minWrNrImp.Item2.ToString("F0"), maxWrNrImp.Item2.ToString("F0"), minWrIrImp.Item2.ToString("F0"), maxWrIrImp.Item2.ToString("F0") };
             logBookDt.Rows.Add(newRow);
             LogBookDataView.ItemsSource = logBookDt.DefaultView;
         }
